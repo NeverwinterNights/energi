55448ed9a47f3b498678ac7f6d978cd7dadfb53c
NeverwinterNights <cardash@mail.ru>
2024-08-28 21:45:34 +0300

    add user and advert page

--- src/components/ui/button/button.stories.tsx
+++ src/components/ui/button/button.stories.tsx
@@ -1,72 +1,72 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from './index'
+import { Button } from "./index";
 
 const meta = {
   argTypes: {
-    onClick: { action: 'clicked' },
+    onClick: { action: "clicked" },
     variant: {
-      control: { type: 'inline-radio-button' },
-      options: ['primary', 'secondary', 'ghost', 'link', 'link-btn'],
+      control: { type: "radio" },
+      options: ["primary", "secondary", "ghost", "link", "link-btn"],
     },
   },
   component: Button,
   parameters: {
     design: {
-      type: 'figma',
-      url: 'https://www.figma.com/file/M7753HAzy0tm9rQWyRBrnI/Inctagram?type=design&node-id=303-3570&mode=dev',
+      type: "figma",
+      url: "https://www.figma.com/file/M7753HAzy0tm9rQWyRBrnI/Inctagram?type=design&node-id=303-3570&mode=dev",
     },
   },
-  tags: ['autodocs'],
-  title: 'UI Components/Button',
-} satisfies Meta<typeof Button>
+  tags: ["autodocs"],
+  title: "UI Components/Button",
+} satisfies Meta<typeof Button>;
 
-export default meta
-type Story = StoryObj<typeof meta>
+export default meta;
+type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    children: 'Primary button',
+    children: "Primary button",
     disabled: false,
-    variant: 'primary',
+    variant: "primary",
   },
-}
+};
 export const Secondary: Story = {
   args: {
-    children: 'Secondary button',
+    children: "Secondary button",
     disabled: false,
-    variant: 'secondary',
+    variant: "secondary",
   },
-}
+};
 export const Ghost: Story = {
   args: {
-    children: 'Ghost button',
+    children: "Ghost button",
     disabled: false,
-    variant: 'ghost',
+    variant: "ghost",
   },
-}
+};
 export const FullWidth: Story = {
   args: {
-    children: 'Full width button',
+    children: "Full width button",
     disabled: false,
     fullWidth: true,
-    variant: 'primary',
+    variant: "primary",
   },
-}
+};
 export const AsLink: Story = {
   args: {
-    as: 'a',
-    children: 'Link as a button',
-    href: '',
-    variant: 'link',
+    as: "a",
+    children: "Link as a button",
+    href: "",
+    variant: "link",
   },
-}
+};
 
 export const LinkAsButton: Story = {
   args: {
-    as: 'a',
-    children: 'Button as link',
-    href: '',
-    variant: 'link-btn',
+    as: "a",
+    children: "Button as link",
+    href: "",
+    variant: "link-btn",
   },
-}
+};
